560bfb7c6fb7a67588ef3f3c3751b391826b074f
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2025-06-02 20:32:58 -0700

    added some blank page reviews and allowed for blank reviews in the app reviews page

--- src/routes/app_reviews/+page.svelte
+++ src/routes/app_reviews/+page.svelte
@@ -14,7 +14,11 @@
             <p>this is a list of some apps that I use or have used, and my thhoughts on them </p>
         </section>
         {#each appReviews as appReview}
-            <h3><a href="{appReview.link}" target="_blank">{appReview.text}</a> {appReview.outsideLink}</h3>
+            {#if appReview.url.slice(0, 5) === "https"}
+                <h3><a href="{appReview.url}" target="_blank">{appReview.text}</a> {appReview.outsideLink}</h3>
+            {:else}
+                <h3>{appReview.text} {appReview.outsideLink}</h3>
+            {/if}
             <p>{appReview.review}</p>
         {/each}
     </div>
